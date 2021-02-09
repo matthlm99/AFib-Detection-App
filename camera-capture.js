b5891bc39fa5e6a577ad46b6315c7a563f8a2638
Matthew <64037899+matthlm99@users.noreply.github.com>
2021-02-09 11:02:23 -0800

    Add files via upload

--- camera-capture.js
+++ camera-capture.js
@@ -1,40 +1,47 @@
 // This script uses the getUserMedia() function to access any inherent camera in the device
 
 // Establishing global variables...
-let preview = document.getElementById("preview");
+let display = document.getElementById("display");
 let recording = document.getElementById("recording");
 let startButton = document.getElementById("startButton");
 let stopButton = document.getElementById("stopButton");
 let downloadButton = document.getElementById("downloadButton");
+let videoMessage = document.getElementById("overlay-message");
 let logElement = document.getElementById("log");
-let recordingTimeMS = 5000;
+let recordingTimeMS = 15000;
 
 // Utility functions
 function wait(delayInMS) {
     return new Promise(resolve => setTimeout(resolve, delayInMS));
 }
 function log(msg) {
-    logElement.innerHTML += msg + "\n";
+    logElement.innerHTML = msg + "\n";
 }
 // Main function for recording a video stream
-function startRecording(stream, lengthInMS) {
-    let recording = new MediaRecorder(stream);
+async function startRecording(stream, lengthInMS) {
+    let recorder = new MediaRecorder(stream);
     let video_data = [];
-  
-    recording.ondataavailable = event => video_data.push(event.data);
-    recording.start();
-    log(recording.state + " for " + (lengthInMS/1000) + " seconds...");
-  
+
+    recorder.ondataavailable = event => video_data.push(event.data);
+    recorder.start();
+    if (recorder.state == "recording"){
+        log(recorder.state + " for " + (lengthInMS/1000) + " seconds...");
+    } else{
+        log("Error; not recording");
+    }
+
     let stopped = new Promise((resolve, reject) => {
-      recording.onstop = resolve;
-      recording.onerror = event => reject(event.name);
+        recorder.onstop = resolve;
+        recorder.onerror = event => reject(event.name);
     });
-  
-    let recorded = wait(lengthInMS).then(() => recording.state == "recording" && recording.stop()
-    );
-  
-    return Promise.all([stopped,recorded])
-    .then(() => data);
+
+    let recorded = wait(lengthInMS).then(function(){
+        recorder.state == "recording" && recorder.stop()
+    });
+    
+    // returns video recording data when promise is resolved
+    await Promise.all([stopped, recorded]);
+    return video_data;
 }
 
 // Video recording stop function
@@ -42,27 +49,34 @@ function stop(stream) {
     stream.getTracks().forEach(track => track.stop());
 }
 
+// Some predefined messages...
+var recording_message = "Recording pulse...";
+var brightness_message = "Room too dark!";
+var pressure_message = "Try pressing more firmly";
 
 // Setting up recording event by the user
 startButton.addEventListener("click", function() {
-    navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
-      preview.srcObject = stream;
-      downloadButton.href = stream;
-      preview.captureStream = preview.captureStream;
-      return new Promise(resolve => preview.onplaying = resolve);
-    }).then(() => startRecording(preview.captureStream(), recordingTimeMS))
-    .then (recordedChunks => {
-      let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
-      recording.src = URL.createObjectURL(recordedBlob);
-      downloadButton.href = recording.src;
-      downloadButton.download = "RecordedVideo.webm";
-  
-      log("Successfully recorded " + recordedBlob.size + " bytes of " +
-          recordedBlob.type + " media.");
-    })
+    navigator.mediaDevices.getUserMedia({video: true}).then(function(stream){
+        videoMessage.innerText = recording_message;
+        display.srcObject = stream;
+        downloadButton.href = stream;
+        display.captureStream = display.captureStream || display.mozCaptureStream;
+        return new Promise(resolve => display.onplaying = resolve);
+        }).then(function(){
+            startRecording(display.captureStream(), recordingTimeMS).then(function(recordedChunks){
+                videoMessage.innerText = "";
+                let recordedBlob = new Blob(recordedChunks, {type: "video/mp4"});
+                let videoURL = window.URL.createObjectURL(recordedBlob);
+                recording.src = videoURL;
+                downloadButton.href = videoURL
+                downloadButton.download = "pulse_video";
+                })
+            })
     .catch(log);
 }, false);
-
+// Event listener for stopping recording
 stopButton.addEventListener("click", function() {
-    stop(preview.srcObject);
+    videoMessage.innerText = "";
+    log("");
+    stop(display.srcObject);
 }, false);
--- image_animation.js
+++ image_animation.js
@@ -1,15 +1,12 @@
 // This script allows for the image to be darkened when it is hovered on
-
-
-const portfolioItems = document.querySelectorAll('.portfolio-img-bg');
-
+const portfolioItems = document.getElementsByClassName(".portfolio-img-bg");
 portfolioItems.foreach(portfolioItem => {
     portfolioItem.addEventListener('mouseover', () => {
         console.log(portfolioItem.childNodes[1].classList);
-        portfolioItem.childNodes[1].classList.add('.img-darken');
+        portfolioItem.childNodes[1].classList.add(".img-darken");
     })
     portfolioItem.addEventListener('mouseout', () => {
         console.log(portfolioItem.childNodes[1].classList);
-        portfolioItem.childNodes[1].classList.remove('.img-darken');
+        portfolioItem.childNodes[1].classList.remove(".img-darken");
     })
 })
